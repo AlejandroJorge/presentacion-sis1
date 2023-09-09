6a2a268da8a4d1765534a697b9b9802266fae9c1
AlejandroJorge <a20202060@pucp.edu.pe>
2023-09-09 17:24:25 -0500

    Terminado el contenido

---
+++ src/components/SvelteComponentt.svelte
@@ -0,0 +1,37 @@
+<script>
+  let count = 0;
+
+  function increment() {
+    count += 1;
+  }
+
+  function decrement() {
+    count -= 1;
+  }
+</script>
+
+<style>
+  .counter {
+    text-align: center;
+    margin: auto;
+    width: 200px;
+    padding: 20px;
+    border: 2px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0 2px 5px #aaa;
+  }
+
+  button {
+    margin: 5px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+</style>
+
+<div class="counter">
+  <h1>Svelte Counter</h1>
+  <p>Count: {count}</p>
+  <button on:click={increment}>Increment</button>
+  <button on:click={decrement}>Decrement</button>
+</div>
